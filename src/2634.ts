538248e9f17bcb29597970feb678348f185d8ceb
Abu Suraj <abusurajbd@gmail.com>
2023-11-17 05:21:14 +0600

    solve 2634. Filter Elements from Array

---
+++ src/2634.ts
@@ -0,0 +1,11 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    const ans:number [] = [];
+	 arr.map((a, i) =>{
+       if(fn(a,i)){
+           ans.push(a)
+       }
+    })
+    return ans;
+};
