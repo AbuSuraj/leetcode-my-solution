6335eece1b76e70041bd2b5a28afca18e69b3854
Abu Suraj <abusurajbd@gmail.com>
2023-12-06 00:48:42 +0600

    add another solution of 1528

--- src/1528.ts
+++ src/1528.ts
@@ -1,10 +1,17 @@
 function restoreString(s: string, indices: number[]): string {
-  let shuffleStr: { [index: number]: string } = []  ;
-  for (let i = 0; i < indices.length; i++) {
-        shuffleStr[indices[i]] = s[i]
-  }
-//   shuffleStr.toString()
-    return shuffleStr.toString().split(',').join('');
+//   let shuffleStr: { [index: number]: string } = []  ;
+//   for (let i = 0; i < indices.length; i++) {
+//         shuffleStr[indices[i]] = s[i]
+//   }
+// //   shuffleStr.toString()
+//     return shuffleStr.toString().split(',').join('');
+let shuffleStr:  string [] = []  ;
+for (let i = 0; i < indices.length; i++) {
+  const idx = indices[i]
+      shuffleStr[indices[i]] = s[i]
+}
+     const ans =  shuffleStr.join('')
+  return ans;
 };
 
 console.log(restoreString("codeleet",  [4,5,6,7,0,2,1,3]))
